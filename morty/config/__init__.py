b60b8592e0f3670338751b41e720a369c45b9550
Roman Glushko <roman.glushko.m@gmail.com>
2021-09-12 13:16:09 +0300

    Renamed args_parser

--- morty/config/__init__.py
+++ morty/config/__init__.py
@@ -1,4 +1,4 @@
-from morty.config.arguments_runner import get_arg_parser
+from morty.config.args_parser import get_arg_parser
 from morty.config.component_factory import ComponentFactory
 from morty.config.decorators import config
 from morty.config.managers import ConfigManager, NotebookConfigManager
